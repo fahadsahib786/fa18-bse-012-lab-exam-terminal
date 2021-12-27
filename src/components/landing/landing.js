bceadc78c1b7f925c77751ba9c473d92a5ba4d88
Muhammad Fahad <fahadsahib786@gmail.com>
2021-12-27 11:38:55 +0500

    lab exam

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -23,11 +23,7 @@ class landing extends Component {
           <Modal show={this.props.authpage} togglepage={this.toggleauthpage}>
             {auth}
           </Modal>
-          {/* <div className="backgroud-video">
-              <video autoPlay loop muted className="video" >
-                <source src='v.mp4' type='video/mp4' />
-              </video>
-          </div> */}
+          
           <div className="heading1">
             <p>To-Do</p>
           </div>
